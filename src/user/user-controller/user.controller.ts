3b531199b245e0aebb2e7db1446a1d94d1d6b358
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-26 14:57:51 +0900

    [Service,Controller] user @Get api 연결

--- src/user/user-controller/user.controller.ts
+++ src/user/user-controller/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { UserService } from '../user-service/user.service';
+import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
+import { User } from '../entity/user.entity';
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get() //경로를 설정하지 않으면 "user/" 경로로 설정이 된다.
+  getHelloWorld(): string {
+    return this.userService.getHelloWorld();
+  }
+}
--- src/user/user-service/user.service.ts
+++ src/user/user-service/user.service.ts
@@ -6,7 +6,20 @@ import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(UserRepository) private UserRepository: UserRepository,
-  ) //  UserService 계층에서 Repository에 접근이 가능합니다.
-  {}
+    @InjectRepository(UserRepository) private UserRepository: UserRepository, //  UserService 계층에서 Repository에 접근이 가능합니다.
+  ) {}
+
+  /**
+   * @author Hamp
+   * @description 유저 생성
+   * @param createUserDto 유저 데이터
+   * @returns {User[]} users
+   **/
+  // onCreateUser(createUserDto: CreateUserDto): Promise<boolean> {
+  //     return this.UserRepository.
+  // }
+
+  getHelloWorld(): string {
+    return 'Hello user World!!';
+  }
 }
