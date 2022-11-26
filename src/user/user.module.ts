9e0dcd87b01e932c708a8756cdfdd1a3d1a0eaed
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-26 13:44:09 +0900

    [CHORE] Repository 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserControllerController } from './user-controller/user-controller.controller';
 import { UserServiceService } from './user-service/user-service.service';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UserControllerController],
-  providers: [UserServiceService]
+  providers: [UserServiceService],
 })
 export class UserModule {}
