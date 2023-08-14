2ee40288b05a93046872faf17ed96bdf9df2a617
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-08-14 23:10:53 +0900

    Update typeorm.config.ts

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'young2518',
+  password: '1234',
   database: 'User',
   entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
   synchronize: true, //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
